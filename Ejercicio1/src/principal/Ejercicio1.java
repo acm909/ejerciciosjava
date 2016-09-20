9641dd7d864c58794da35c38fe56fab7e662aea9
acm909 <antoniocaballero.909@gmail.com>
2016-09-20 21:28:31 +0200

    modificacion add comentario en ejercicio1

--- Ejercicio1/src/principal/Ejercicio1.java
+++ Ejercicio1/src/principal/Ejercicio1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
 
-  
+  //Ejercicio numero primo
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
         System.out.println("Introduzca un numero");
